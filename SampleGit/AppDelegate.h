fc1084d0d8a8eb3233b8c3dc5d299e1812970690
Girish purandare <Sysfort@girishs-mac-mini-3.local>
2018-10-22 12:05:43 +0530

    Initial Commit

---
+++ SampleGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleGit
+//
+//  Created by Girish purandare on 10/22/18.
+//  Copyright © 2018 Sysfort Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleGit
+//
+//  Created by Girish purandare on 10/22/18.
+//  Copyright © 2018 Sysfort Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
